9f8c44b90c0fe15d29c95c565495dd7034c84727
Darren Shepherd <darren@rancher.com>
2022-04-15 15:10:50 -0700

    Add tee

--- pkg/typed/chan.go
+++ pkg/typed/chan.go
@@ -12,3 +12,19 @@ func Debounce[T any](in <-chan T) <-chan T {
 	}()
 	return result
 }
+
+func Tee[T any](in <-chan T) (<-chan T, <-chan T) {
+	one := make(chan T, 1)
+	two := make(chan T, 1)
+
+	go func() {
+		defer close(one)
+		defer close(two)
+		for x := range in {
+			one <- x
+			two <- x
+		}
+	}()
+
+	return one, two
+}
