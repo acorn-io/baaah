4f9a6dc02bf1db75599109e8b485bf9acbdda891
Darren Shepherd <darren@acorn.io>
2022-10-14 14:11:28 -0700

    Add indexable fields
    Signed-off-by: Darren Shepherd <darren@acorn.io>

--- pkg/lasso/backend.go
+++ pkg/lasso/backend.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/acorn-io/baaah/pkg/backend"
+	"github.com/acorn-io/baaah/pkg/fields"
 	"github.com/acorn-io/baaah/pkg/router"
 	"github.com/acorn-io/baaah/pkg/uncached"
+	controllerruntime "github.com/rancher/lasso/controller-runtime"
 	"github.com/rancher/lasso/pkg/controller"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	kcache "k8s.io/client-go/tools/cache"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
@@ -22,7 +26,7 @@ type Backend struct {
 
 	cacheFactory controller.SharedControllerFactory
 	cache        cache.Cache
-	started      bool
+	started      atomic.Bool
 }
 
 func newBackend(cacheFactory controller.SharedControllerFactory, client *cacheClient, cache cache.Cache) *Backend {
@@ -33,7 +37,12 @@ func newBackend(cacheFactory controller.SharedControllerFactory, client *cacheCl
 	}
 }
 
-func (b *Backend) Start(ctx context.Context) error {
+func (b *Backend) Start(ctx context.Context) (err error) {
+	defer func() {
+		if err == nil {
+			b.started.Store(true)
+		}
+	}()
 	if err := b.cacheFactory.Start(ctx, 5); err != nil {
 		return err
 	}
@@ -67,16 +76,64 @@ func (b *Backend) Trigger(gvk schema.GroupVersionKind, key string, delay time.Du
 	return nil
 }
 
+func (b *Backend) addIndexer(ctx context.Context, gvk schema.GroupVersionKind) error {
+	obj, err := b.Scheme().New(gvk)
+	if err != nil {
+		return err
+	}
+	f, ok := obj.(fields.Fields)
+	if !ok {
+		return nil
+	}
+	caches := b.cacheFactory.SharedCacheFactory().WaitForCacheSync(ctx)
+	if caches[gvk] {
+		return nil
+	}
+
+	cache, err := b.cacheFactory.SharedCacheFactory().ForKind(gvk)
+	if err != nil {
+		return err
+	}
+
+	indexers := map[string]kcache.IndexFunc{}
+	for _, field := range f.FieldNames() {
+		field := field
+		indexers["field:"+field] = kcache.IndexFunc(func(obj interface{}) ([]string, error) {
+			f, ok := obj.(fields.Fields)
+			if !ok {
+				return nil, nil
+			}
+			v := f.Get(field)
+			if v == "" {
+				return nil, nil
+			}
+			vals := []string{controllerruntime.KeyToNamespacedKey("", v)}
+			if ko, ok := obj.(kclient.Object); ok && ko.GetNamespace() != "" {
+				vals = append(vals, controllerruntime.KeyToNamespacedKey(ko.GetNamespace(), v))
+			}
+			return vals, nil
+		})
+	}
+	return cache.AddIndexers(indexers)
+}
+
 func (b *Backend) Watch(ctx context.Context, gvk schema.GroupVersionKind, name string, cb backend.Callback) error {
 	c, err := b.cacheFactory.ForKind(gvk)
 	if err != nil {
+		return err
+	}
+	if err := b.addIndexer(ctx, gvk); err != nil {
 		return err
 	}
 	handler := controller.SharedControllerHandlerFunc(func(key string, obj runtime.Object) (runtime.Object, error) {
 		return cb(gvk, key, obj)
 	})
 	c.RegisterHandler(ctx, fmt.Sprintf("%s %v", name, gvk), handler)
-	return c.Start(ctx, 5)
+
+	if b.started.Load() {
+		return c.Start(ctx, 5)
+	}
+	return nil
 }
 
 func (b *Backend) GVKForObject(obj runtime.Object, scheme *runtime.Scheme) (schema.GroupVersionKind, error) {
