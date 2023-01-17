4b059c431d06468bb4685f637fce93547f02a1bc
Darren Shepherd <darren@acorn.io>
2023-01-17 12:54:44 -0700

    Add SafeConcatName to baaah

---
+++ pkg/name/name.go
@@ -0,0 +1,27 @@
+package name
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+)
+
+func SafeConcatNameWithSeparatorAndLength(length int, sep string, name ...string) string {
+	fullPath := strings.Join(name, "-")
+	if len(fullPath) < length {
+		return fullPath
+	}
+	digest := sha256.Sum256([]byte(fullPath))
+	// since we cut the string in the middle, the last char may not be compatible with what is expected in k8s
+	// we are checking and if necessary removing the last char
+	c := fullPath[length-8]
+	if 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
+		return fullPath[0:length-7] + "-" + hex.EncodeToString(digest[0:])[0:5]
+	}
+
+	return fullPath[0:length-8] + "-" + hex.EncodeToString(digest[0:])[0:6]
+}
+
+func SafeConcatName(name ...string) string {
+	return SafeConcatNameWithSeparatorAndLength(64, "-", name...)
+}
