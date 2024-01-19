2a58ee757bbdeb7a1ec3faaa60929d5dd6e232e3
Darren Shepherd <darren@acorn.io>
2024-01-19 09:03:09 -0700

    Fix type errors from cached unstructured object
    The cacheClient may have saved in memory an unstructured object which is
    not assignable to the native struct type. In this situation do JSON
    marshaling
    
    Signed-off-by: Darren Shepherd <darren@acorn.io>

--- pkg/runtime/copy.go
+++ pkg/runtime/copy.go
@@ -1,13 +1,22 @@
 package runtime
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func CopyInto(dst, src runtime.Object) error {
+	if _, ok := src.(*unstructured.Unstructured); ok {
+		data, err := json.Marshal(src)
+		if err != nil {
+			return err
+		}
+		return json.Unmarshal(data, dst)
+	}
 	src = src.DeepCopyObject()
 	dstVal := reflect.ValueOf(dst)
 	srcVal := reflect.ValueOf(src)
